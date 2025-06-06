343fa63d525f1f36904a0e7b536953b7fe75c002
shuvam-bhatt <shuvam bhatt>
2025-06-07 02:12:18 +0530

    loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
-import 'package:flutter_catalog/login_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,17 +14,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      initialRoute: "/home", //for the initial routing
-      routes: {
-        "/": (context)=> HomePage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
-      },
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple, 
+        fontFamily: GoogleFonts.lato().fontFamily,
+        ),
 
+        debugShowCheckedModeBanner: false ,
+
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/", //for the initial routing
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+      },
     );
   }
 }
