aba5cff2389d344c8fb1bd96e971d6e30c5d33f9
Shuvam Bhatt <shuvambhattsb2021@gmail.com>
2025-06-15 23:46:02 +0530

    gridview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,16 +13,59 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: Text("Catalog App")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)? ListView.builder(
-          //builder gives recycler view
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(item: CatalogModel.items[index]);
-          },
-        )
-        : Center(
-          child: CircularProgressIndicator(),
-        )
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            // Uncomment the below section to use ListView instead of GridView
+            /*
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  return ItemWidget(item: CatalogModel.items[index]);
+                },
+              )
+            */
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                ),
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: GridTile(
+                      header: Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                        child: Text(
+                          item.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      child: Image.network(item.image),
+                      footer: Container(
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        child: Text(
+                          item.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
